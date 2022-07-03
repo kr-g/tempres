70e4b9c387c199e80f8fbbcfc2284b25720332b6
kr-g <k.r.goger@gmail.com>
2022-07-03 09:39:45 +0200

    stubs for station management (openweathermap)

--- tempres/mainpub.py
+++ tempres/mainpub.py
@@ -8,6 +8,8 @@ from urllib.request import urlopen
 
 from pyjsoncfg import Config
 
+VERSION = "v0.0.0.1"
+
 DEFAULT_PATH = "~/.tempres/"
 
 CONFIG_FNAM = "stations.json"
@@ -15,6 +17,76 @@ CONFIG_FNAM = "stations.json"
 BASE_URL = "http://api.openweathermap.org"
 GEO_VER = "1.0"
 DATA_VER = "2.5"
+STATION_VER = "3.0"
+
+#
+
+
+def build_station_url(api_key, station_id=None):
+    if station_id is None:
+        station_id = ""
+    else:
+        station_id = "/" + station_id
+    return f"{BASE_URL}/data/{STATION_VER}/stations{station_id}?appid={api_key}"
+
+
+# def build_push_station_url():
+#     return f"{BASE_URL}/data/{STATION_VER}/measurements?appid={api_key}"
+
+# api doc
+# https://openweathermap.org/stations
+def request_stations(api_key, station_id=None, is_ext_id=True):
+    url = build_station_url(api_key, station_id if not is_ext_id else None)
+    debug and print("url", url)
+    resp = urlopen(url)
+    if resp.status != 200:
+        raise Exception(f"failed to load {url} with error {resp.status}")
+    cont = resp.read()
+    jso = json.loads(cont)
+    if station_id and is_ext_id:
+        jso = list(filter(lambda x: x["external_id"] == station_id, jso))
+    return jso
+
+
+def register_update_station(station_id, name, lat, lon, alt, api_key):
+
+    raise Exception("untested")
+
+    stations = request_stations(api_key, station_id, False)
+    update = len(stations) > 0
+
+    data = {
+        "external_id": station_id,
+        "name": name,
+        "latitude": lat,
+        "longitude": lon,
+    }
+    if alt is not None:
+        data.update({"altitude": alt})
+
+    id = stations[0]["id"] if len(stations) > 0 else None
+
+    data = json.dumps(data)
+    url = build_station_url(api_key, id)
+    resp = urlopen(url, data=data, method="POST" if update is False else None)
+    if resp.status != 204:
+        raise Exception(f"failed to load {url} with error {resp.status}")
+    return resp
+
+
+def delele_station(station_id, api_key):
+
+    raise Exception("untested")
+
+    stations = request_stations(api_key, station_id, False)
+    url = build_station_url(api_key, station_id)
+    resp = urlopen(url, data=data, method="DELETE")
+    if resp.status != 200:
+        raise Exception(f"failed to load {url} with error {resp.status}")
+    return resp
+
+
+#
 
 
 def read_conf():
@@ -59,12 +131,10 @@ def main_func():
         description="interface to OpenWeatherMap",
     )
     parser.add_argument(
-        "-v",
         "--version",
-        dest="show_version",
-        action="store_true",
-        help="show version info and exit",
-        default=False,
+        "-v",
+        action="version",
+        version=f"%(prog)s {VERSION}",
     )
     parser.add_argument(
         "-debug",
@@ -108,7 +178,25 @@ def main_func():
         "-raw",
         dest="raw",
         action="store_true",
-        help="returns raw data",
+        help="returns raw data  (default: %(default)s)",
+        default=False,
+    )
+
+    list_group = parser.add_argument_group("list", "list options")
+    list_group.add_argument(
+        "-list-stations",
+        "-ls",
+        dest="station_list",
+        action="store_true",
+        help="list all stations, or station with '-id'",
+        default=False,
+    )
+    list_group.add_argument(
+        "-is-owm-id",
+        "-isid",
+        dest="station_no_ext_id",
+        action="store_true",
+        help="true if id is open weather map id, or station_id from config file (default: %(default)s)",
         default=False,
     )
 
@@ -120,15 +208,16 @@ def main_func():
 
     debug = args.debug
 
-    if args.show_version:
-        print("Version:", "not-yet")
-        return
-
     cfg = read_conf()
     cfg_ = cfg()
 
     api_key = args.api_key if args.api_key is not None else cfg_.api_key
     debug and print("api_key", api_key)
+
+    if args.station_list:
+        rc = request_stations(api_key, args.station_id, not args.station_no_ext_id)
+        print(rc)
+        return
 
     st = list(filter(lambda x: x.station_id == args.station_id, cfg_.stations))
     debug and print(st)
